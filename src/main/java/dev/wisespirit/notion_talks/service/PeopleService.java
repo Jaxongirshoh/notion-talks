33a4f7e39d85868efda560389f73fd9c5448c4ad
wisespirit <@Jaxongirshoh>
2025-01-12 00:47:58 +0500

    did something

--- src/main/java/dev/wisespirit/notion_talks/service/PeopleService.java
+++ src/main/java/dev/wisespirit/notion_talks/service/PeopleService.java
@@ -8,7 +8,7 @@ public class PeopleService {
     public static People mapPaeToPeople(Page page){
         return new People(
                 page.getId(),
-                page.getProperties().get("Name").get("title").get(0).get("text").get("content").asText(),
+                page.getProperties().get("Name").get("name").get(0).get("text").get("content").asText(),
                 page.getProperties().get("rich_text").get(0).get("text").get("content").asText(),
                 page.getProperties().get("Email").get("email").asText(),
                 page.getProperties().get("URL").get("url").asText()
